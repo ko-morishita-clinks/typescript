1011bab9f5a6155a55f31910b68160ee39bdbb95
ko-morishita-clinks <ko-morishita@clinks.jp>
2025-09-24 14:35:24 +0900

    feat: sec4

--- src/index.js
+++
@@ -1,4 +0,0 @@
-var mass = 60;
-var height = 1.7;
-var bmi = mass / (height * height);
-console.log("BMI: ".concat(bmi));
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,1 @@
-const mass = 60;
-const height = 1.7;
-const bmi = mass / (height * height);
-console.log(`BMI: ${bmi}`);
+console.log('Hello index.ts!!!');
